63e3648f680d754bd65645929905e5f915fb150b
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-16 22:55:14 +0330

    change metric loss to cosface loss

--- losses.py
+++ losses.py
@@ -12,7 +12,8 @@ class MetricLoss(torch.nn.Module):
     self.miner = miners.BatchEasyHardMiner() # for ContrastiveLoss 
     # self.metric = losses.NTXentLoss(temperature=0.07)
     # self.metric = losses.ContrastiveLoss(pos_margin=0, neg_margin=1)
-    self.metric = losses.TripletMarginLoss(margin=0.05)
+    # self.metric = losses.TripletMarginLoss(margin=0.05)
+    self.metric = losses.CosFaceLoss(args.n_classes, args.hidden_dims, margin=0.35, scale=64)
     
     
   def forward(self, logits, labels):
