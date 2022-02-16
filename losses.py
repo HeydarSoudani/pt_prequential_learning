5d90e780062c9540fd42e17c21c7d4aa0d2bddd5
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-16 22:36:18 +0330

    change metric loss

--- losses.py
+++ losses.py
@@ -9,7 +9,7 @@ class MetricLoss(torch.nn.Module):
     self.lambda_2 = args.lambda_2 # CE coef
 
     self.ce = torch.nn.CrossEntropyLoss()
-    # self.miner = miners.BatchEasyHardMiner() # for ContrastiveLoss 
+    self.miner = miners.BatchEasyHardMiner() # for ContrastiveLoss 
     self.metric = losses.NTXentLoss(temperature=0.07)
     # self.metric = losses.ContrastiveLoss(pos_margin=0, neg_margin=1)
     # self.metric = losses.TripletMarginLoss(margin=0.05)
@@ -19,11 +19,11 @@ class MetricLoss(torch.nn.Module):
     cls_loss = self.ce(logits, labels.long())
     
     # loss with miner
-    # miner_output = self.miner(logits, labels.long())
-    # metric_loss = self.metric(logits, labels.long(), miner_output)
+    miner_output = self.miner(logits, labels.long())
+    metric_loss = self.metric(logits, labels.long(), miner_output)
 
     # loss without minier
-    metric_loss = self.metric(logits, labels.long())
+    # metric_loss = self.metric(logits, labels.long())
 
     return self.lambda_1 * metric_loss +\
            self.lambda_2 * cls_loss
