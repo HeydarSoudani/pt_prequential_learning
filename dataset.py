ef42128a2d897c756a280c71830eaecac25d9917
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-17 01:37:34 +0330

    add dataset param

--- dataset.py
+++ dataset.py
@@ -19,14 +19,15 @@ class ChunkDataset(Dataset):
     #   self.tensor_view = (3, 32, 32)
     self.transforms = transforms
     self.data = []
-    self.labels = np.argmax(data[:, -10:], axis=1)
+    self.labels = np.argmax(data[:, -1], axis=1)
     self.label_set = set(self.labels)
   
     for idx, s in enumerate(data):
       # x = (tensor(s[:-1], dtype=torch.float) / 255).view(self.tensor_view)
-      # y = tensor(self.labels[idx], dtype=torch.long)
-      x = (tensor(s[:-10], dtype=torch.float) / 255)
-      y = tensor(onehot2index(s[-10:]), dtype=torch.long)
+      # y = tensor(onehot2index(s[-10:]), dtype=torch.long)
+      x = (tensor(s[:-1], dtype=torch.float) / 255)
+      y = tensor(self.labels[idx], dtype=torch.long)
+      
       self.data.append((x, y))
 
   def __getitem__(self, index):
